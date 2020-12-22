c1d0bc586abdc39202e136715759219f973249ff
Bogdan Nedelcu <bogdan.nedelcu@enpal.de>
2020-12-22 17:50:02 +0100

    added deploy script

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
-const port = 3000;
+const port = 8080;
 
 app.listen(port, () =>
-  console.log(`My first server is runnnin at http://localhost:${port}`)
+  console.log(`Server is runing at http://localhost:${port}`)
 );
