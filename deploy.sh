2063b80705b0f80dfdd54f0e5a968a23c2d6dffc
Bogdan Nedelcu <bogdan.nedelcu@enpal.de>
2020-12-23 08:43:01 +0100

    updated deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
+# installing dependencies
 npm install
 npm install pm2 -g
-sudo pm2 start app.js
+
+# clean up old app
+pm2 stop app
+pm2 delete app
+
+# start app with pm2
+sudo pm2 start app.js --name "app"
